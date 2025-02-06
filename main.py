23d695253c802e066db69a61c62c4fa708911519
Chathura-Jayasinghe <jayasinghejmcnb.21@uom.lk>
2025-02-06 18:55:59 +0530

    added some features

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_q:
+               running = False   
 
     # Get key presses
     keys = pygame.key.get_pressed()
